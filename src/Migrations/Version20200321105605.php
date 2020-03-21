a162494e3e09cc7c18deaa0f9d41340349734b3a
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-03-21 13:27:57 +0100

    add folder front html css

---
+++ src/Migrations/Version20200321105605.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200321105605 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, publi_id INT DEFAULT NULL, user_id INT DEFAULT NULL, type INT NOT NULL, name VARCHAR(50) NOT NULL, path VARCHAR(255) NOT NULL, is_active TINYINT(1) NOT NULL, date_register DATE NOT NULL, date_update DATE NOT NULL, date_delete DATE NOT NULL, INDEX IDX_C53D045FB1F221C3 (publi_id), INDEX IDX_C53D045FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045FB1F221C3 FOREIGN KEY (publi_id) REFERENCES publication (id)');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045FA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE image');
+    }
+}
