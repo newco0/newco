d1206dd5a38c51d81a30ade1ed67528090fbaba6
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-03-21 14:00:46 +0100

    add skeleton index php

---
+++ html/front/header.php
@@ -0,0 +1,19 @@
+<header class="col-3">
+    <div class="w-75 mx-auto">
+        <a href="#"><img src="../img/logo.png" class="img-fluid" alt=""></a>
+    </div>
+    <ul class="w-75 mt-5 p-0 mx-auto text-center listmenu">
+        <a href="#">
+            <li class="list-unstyled p-2 font-weight-bold menulink">Accueil</li>
+        </a>
+        <a href="#">
+            <li class="list-unstyled p-2 font-weight-bold menulink">Message</li>
+        </a>
+        <a href="#">
+            <li class="list-unstyled p-2 font-weight-bold menulink">Publier</li>
+        </a>
+        <a href="#">
+            <li class="list-unstyled p-2 font-weight-bold menulink">Contact</li>
+        </a>
+    </ul>
+</header>
---
+++ html/front/rightside.php
@@ -0,0 +1,6 @@
+<div class="col-3">
+    <form class="form-inline">
+        <input class="form-control w-75 mr-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
+    </form>
+</div>
