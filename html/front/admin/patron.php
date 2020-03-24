404b71f4dd957d161c1f7caf1de012c6dcbded33
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-03-24 13:53:27 +0100

    start admin

---
+++ html/front/admin/patron.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="stylesheet" href="style_admin.css">
+    <title>Title</title>
+</head>
+
+<body>
+
+    <header class="container-fluid">
+        <div class="row">
+            <div class="mx-auto d-flex justify-content-center">
+                <div class="mr-5">
+                    <a href="index.php"><img src="../../img/logo.png" class="img-fluid" alt=""></a>
+                </div>
+                <div class="d-flex align-items-center">
+                    <nav class="">
+                        <ul class="list-unstyled d-flex border nav-height">
+                            <li class="mx-2 border-right"><a class="text-black text-dark" href="index.php">Accueil</a></li>
+                            <li class="mx-2 border-right"><a class="text-dark my-auto" href="users.php">Utilisateurs</a></li>
+                            <li class="mx-2 border-right"><a class="text-dark my-auto" href="users.php">Messages</a></li>
+                            <li class="mx-2 border-right"><a class="text-dark my-auto" href="users.php">Administrateurs</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="d-flex align-items-center ml-5">
+                    <img class="arround rounded-circle mr-2" src="../../img/orang.jpg" alt="photo de profil">
+                </div>
+                <div class="d-flex align-items-center ml-5">
+                    <i class="fas fa-sign-out-alt fa-3x"></i>
+                </div>
+            </div>
+        </div>
+    </header>
+    <script src="https://kit.fontawesome.com/a2bab1df4f.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
