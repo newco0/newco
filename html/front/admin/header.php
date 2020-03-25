8e683b2051c7893c2649dbdd8c37cdc0d8b5ca81
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-03-25 13:05:44 +0100

    petite modif header

--- html/front/admin/header.php
+++ html/front/admin/header.php
@@ -9,9 +9,9 @@
             <div class="col-3 mx-auto col-lg-2 d-flex justify-content-center align-items-center p-0">
                 <a href="index.php"><img src="../../img/logo.png" class="w-100 col-4 img-fluid logo p-0" alt=""></a>
             </div> 
-            <div class="menu col-lg-6 d-md-flex align-items-center w-50">
+            <div class="menu col-lg-6 d-lg-flex align-items-center w-50">
                 <nav class="mx-auto">
-                    <ul class="list-unstyled d-md-flex">
+                    <ul class="list-unstyled d-lg-flex">
                         <li class="p-2"><a class="text-white textanch" href="index.php">Accueil</a></li>
                         <li class="p-2"><a class="text-white textanch" href="users.php">Utilisateurs</a></li>
                         <li class="p-2"><a class="text-white textanch" href="users.php">Messages</a></li>
