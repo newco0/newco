f1898a54a98712f68c195114e62941757b33d733
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-03-25 08:50:44 +0100

    header admin

--- html/front/admin/header.php
+++ html/front/admin/header.php
@@ -1,15 +1,25 @@
 <header class="container-fluid p-0">
     <div class="row w-100 w75 mx-xl-auto m-0 p-2">
-        <div class="w-100 mx-md-auto d-flex justify-content-between text-lg-center">
+        <div class="w-100 mx-md-auto d-flex justify-content-between text-lg-center position-relative">
             <div class="col-2 p-2 d-block d-lg-none my-auto hamb">
                 <div class="hamb1 bg-dark mb-2"></div>
                 <div class="hamb1 bg-dark mb-2"></div>
                 <div class="hamb1 bg-dark"></div>
-            </div> 
+            </div>
+            <div class="menu col-lg-6 d-lg-flex align-items-center w-50">
+                <nav class="mx-auto">
+                    <ul class="list-unstyled">
+                        <li class="p-2"><a class="text-white" href="index.php">Accueil</a></li>
+                        <li class="p-2"><a class="text-white" href="users.php">Utilisateurs</a></li>
+                        <li class="p-2"><a class="text-white" href="users.php">Messages</a></li>
+                        <li class="p-2"><a class="text-white my-auto" href="users.php">Administrateurs</a></li>
+                    </ul>
+                </nav>
+            </div>
             <div class="col-3 col-lg-2">
                 <a href="index.php"><img src="../../img/logo.png" class="w-100 col-4 img-fluid logo" alt=""></a>
             </div>
-            <div class="dnone col-lg-6 d-lg-flex align-items-center menu">
+            <div class="d-none col-lg-6 d-lg-flex align-items-center">
                 <nav class="mx-auto">
                     <ul class="list-unstyled d-flex nav-height border">
                         <li class="p-2 border-right d-flex align-items-center"><a class="text-dark" href="index.php">Accueil</a></li>
