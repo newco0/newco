d8553c508c79910446988beca3d064dcf269a9cd
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-03-25 13:15:29 +0100

    delete espace en trop

--- html/front/admin/admin.js
+++ html/front/admin/admin.js
@@ -1,71 +1,53 @@
-$(document).ready(function() {
-  $(".hamb").click(function() {
-    $(".menu").toggle("slow");
-  });
+$(document).ready(function () {
+    $(".hamb").click(function () {
+        $(".menu").toggle("slow");
+    });
 
-  $(".imgprofil").mouseenter(function() {
-    $(this)
-      .next()
-      .next()
-      .css("opacity", 1);
-  });
+    $(".imgprofil").mouseenter(function () {
+        $(this)
+            .next()
+            .next()
+            .css("opacity", 1);
+    });
 
-  $(".imgprofil").mouseleave(function() {
-    $(this)
-      .next()
-      .next()
-      .css("opacity", 0);
-  });
+    $(".imgprofil").mouseleave(function () {
+        $(this)
+            .next()
+            .next()
+            .css("opacity", 0);
+    });
 
-  $(".imgcouv").mouseenter(function() {
-    $(this)
-      .next()
-      .next()
-      .css("opacity", 1);
-  });
+    $(".imgcouv").mouseenter(function () {
+        $(this)
+            .next()
+            .next()
+            .css("opacity", 1);
+    });
 
-  $(".imgcouv").mouseleave(function() {
-    $(this)
-      .next()
-      .next()
-      .css("opacity", 0);
-    $(this)
-      .parent()
-      .css("opacity", 1);
-  });
+    $(".imgcouv").mouseleave(function () {
+        $(this)
+            .next()
+            .next()
+            .css("opacity", 0);
+        $(this)
+            .parent()
+            .css("opacity", 1);
+    });
 
-  $(".editimgprofil").mouseenter(function() {
-    console.log("coucou");
-    $(this).css("opacity", 1);
-  });
+    $(".editimgprofil").mouseenter(function () {
+        console.log("coucou");
+        $(this).css("opacity", 1);
+    });
 
-  $(".fileprofil").mouseenter(function() {
-    $(this)
-      .next()
-      .css("opacity", 1);
-    $(this)
-      .prev()
-      .css("opacity", 0.5);
-  });
+    $(".fileprofil").mouseenter(function () {
+        $(this)
+            .next()
+            .css("opacity", 1);
+        $(this)
+            .prev()
+            .css("opacity", 0.5);
+    });
 
-  $(".fileprofil").mouseleave(function() {
-    $(this)
-      .next()
-      .css("opacity", 0);
-    $(this)
-      .prev()
-      .css("opacity", 1);
-  });
-
-  $(".messagesendform").submit(function(e) {
-    e.preventDefault();
-    $(".messageshow").append(
-      `<p class="bgcolor68c2e8 rounded text-white p-2 w-75 mx-auto">${$(
-        ".responsemessage"
-      ).val()}</p>`
-    );
-    $(".responsemessage").val("");
-  });
     $(".fileprofil").mouseleave(function () {
         $(this)
             .next()
@@ -75,15 +57,33 @@ $(document).ready(function() {
             .css("opacity", 1);
     });
 
-    $('.messagesendform').submit(function(){
+    $(".messagesendform").submit(function (e) {
+        e.preventDefault();
+        $(".messageshow").append(
+            `<p class="bgcolor68c2e8 rounded text-white p-2 w-75 mx-auto">${$(
+                ".responsemessage"
+            ).val()}</p>`
+        );
+        $(".responsemessage").val("");
+    });
+    $(".fileprofil").mouseleave(function () {
+        $(this)
+            .next()
+            .css("opacity", 0);
+        $(this)
+            .prev()
+            .css("opacity", 1);
+    });
+
+    $('.messagesendform').submit(function () {
 
     })
 
-    $('.name').click(function(){
-        $('.text').toggle( "slow");
+    $('.name').click(function () {
+        $('.text').toggle("slow");
     });
 
-    $('.zoom').click(function(){
+    $('.zoom').click(function () {
         $('.zoomer').removeClass('d-none');
         $('.zoom').removeClass('col-3', 'col-lg-4', 'col-xl-3')
         $('.zoomer').append($(this));
