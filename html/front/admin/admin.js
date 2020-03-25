094d8a8fab8eada3486ca36f70fd1d838e07b448
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-03-25 08:19:56 +0100

    header

--- html/front/admin/admin.js
+++ html/front/admin/admin.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    
+    $('.hamb').click(function(){
+        console.log('coucou')
+        $('.menu').toggle()
+    })
 });
--- html/front/admin/header.php
+++ html/front/admin/header.php
@@ -1,16 +1,16 @@
 <header class="container-fluid p-0">
-    <div class="row w-100 m-0 p-2">
+    <div class="row w-100 w75 mx-xl-auto m-0 p-2">
         <div class="w-100 mx-md-auto d-flex justify-content-between text-lg-center">
-            <div class="col-2 d-block d-lg-none my-auto">
+            <div class="col-2 p-2 d-block d-lg-none my-auto hamb">
                 <div class="hamb1 bg-dark mb-2"></div>
                 <div class="hamb1 bg-dark mb-2"></div>
                 <div class="hamb1 bg-dark"></div>
             </div> 
-            <div class="col-3">
+            <div class="col-3 col-lg-2">
                 <a href="index.php"><img src="../../img/logo.png" class="w-100 col-4 img-fluid logo" alt=""></a>
             </div>
-            <div class="d-none d-lg-flex align-items-center">
-                <nav class="">
+            <div class="dnone col-lg-6 d-lg-flex align-items-center menu">
+                <nav class="mx-auto">
                     <ul class="list-unstyled d-flex nav-height border">
                         <li class="p-2 border-right d-flex align-items-center"><a class="text-dark" href="index.php">Accueil</a></li>
                         <li class="p-2 border-right d-flex align-items-center"><a class="text-dark" href="users.php">Utilisateurs</a></li>
@@ -19,9 +19,9 @@
                     </ul>
                 </nav>
             </div>
-            <div class="col-3 d-flex align-items-center">
+            <div class="col-6 col-lg-4 d-flex justify-content-around align-items-center">
                 <div class="mr-3">
-                    <img class="arround rounded-circle w-100" src="../../img/orang.jpg" alt="photo de profil">
+                    <img class="col-4 arround rounded-circle w-100 p-0" src="../../img/orang.jpg" alt="photo de profil">
                 </div>
                 <div class="mr-3">
                     <i class="fas fa-sign-out-alt fa-3x mx-auto"></i>
